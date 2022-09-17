cc0657b14bac6d4d0993ba7b037ae5076fbbd072
Victor110901 <victor.cs2001.vc@gmail.com>
2022-09-16 23:58:51 -0300

    Rota para LOGIN

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './component/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavComponent } from './components/nav/nav.component';
 import { TecnicoListComponent } from './tecnico/tecnico-list/tecnico-list.component';
 
 const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
   {
     path: '',
     component: NavComponent,
