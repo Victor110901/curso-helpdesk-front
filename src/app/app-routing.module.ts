1b4f7a8d5acead736256722e0085b7f726e6f760
Victor110901 <victor.cs2001.vc@gmail.com>
2022-09-18 22:19:59 -0300

    Rota para componente CHAMADO-UPDATE

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,7 @@ import { ClienteCreateComponent } from './components/cliente/cliente-create/clie
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ChamadoListComponent } from './components/chamado/chamado-list/chamado-list.component';
 import { ChamadoCreateComponent } from './components/chamado/chamado-create/chamado-create.component';
+import { ChamadoUpdateComponent } from './components/chamado/chamado-update/chamado-update.component';
 
 const routes: Routes = [
   {
@@ -70,6 +71,10 @@ const routes: Routes = [
       {
         path: 'chamados/create',
         component: ChamadoCreateComponent
+      },
+      {
+        path: 'chamados/update/:id',
+        component: ChamadoUpdateComponent
       }
     ]
   },
